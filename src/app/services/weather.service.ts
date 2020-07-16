117cef324ada50239273a65bdbcebff99de6bf1c
Jesus Lopez <jesuselopez123@gmail.com>
2020-07-16 11:00:50 -0400

    Se elimina comentarios

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -16,12 +16,7 @@ export class WeatherService {
   constructor(
     private _http: HttpClient
   ) { 
-    this.url = `http://api.worldweatheronline.com/premium/v1/weather.ashx?q=`;
-    // this.url = 'http://dummy.restapiexample.com/api/v1/employees';
-    // this.url = `http://api.worldweatheronline.com/premium/v1/weather.ashx?q=London,united+kingdom&key=186abc310518457cbf2180608201507&format=json`;
-
-
-    
+    this.url = `http://api.worldweatheronline.com/premium/v1/weather.ashx?q=`;    
   }
   getWeather(cityName:any, countryCode: string): Observable<any>{
     
